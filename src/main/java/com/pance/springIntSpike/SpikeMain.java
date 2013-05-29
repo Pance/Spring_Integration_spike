a221e5550435cdbc23a4b4dd30349f7af5746383
Pance <pbarnett888@gmail.com>
2013-05-29 11:52:16 -0500

    Changed 'out' channel to a queue so that it can be polled and we can get a message out of it

--- src/main/java/com/pance/springIntSpike/SpikeMain.java
+++ src/main/java/com/pance/springIntSpike/SpikeMain.java
@@ -8,9 +8,10 @@ public class SpikeMain {
 
 	public static void main(String[] args) {
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring-context.xml", SpikeMain.class);
-		MessageChannel inputChannel = context.getBean("inputChannel", MessageChannel.class);
-		PollableChannel outputChannel = context.getBean("outputChannel", PollableChannel.class);
-		inputChannel.send(new GenericMessage<String>("blah"));
-		System.out.println("got: " + outputChannel.receive(0).getPayload());
+		MessageChannel fooChannel = context.getBean("foo", MessageChannel.class);
+		PollableChannel outChannel = context.getBean("out", PollableChannel.class);
+		
+		fooChannel.send(new GenericMessage<String>("blah"));
+		System.out.println("got: " + outChannel.receive(0).getPayload());
 	}
 }
